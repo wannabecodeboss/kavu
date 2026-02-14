adfa332812fe96f06afe59c7a3bc53abf40cbb18
Hiteshi Meisheri <152746571+wannabecodeboss@users.noreply.github.com>
2026-02-15 01:15:47 +0530

    Add more images to the slideshow array

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // slideshow
 const images = [
-  "images/img1.jpg",
-  "images/img2.jpg",
-  "images/img3.jpg"
+  "img1.jpg",
+  "img2.jpg",
+  "img3.jpg"
+  "img4.jpg"
+  "img5.jpg"
+  "img6.jpg"
+  "img7.jpg"
+  "img8.jpg"
+  "img9.jpg"
+  "img10.jpg"
+  "img11.jpg"
+  "img12.jpg"
+  "img13.jpg"
+  "img14.jpg"
+  "img15.jpg"
+  "img16.jpg"
+  "img17.jpg"
+  "img18.jpg"
+  "img19.jpg"
+  "img20.jpg"
+  "img21.jpg"
 ];
 
 let i = 0;
